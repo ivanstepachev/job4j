73a508e499e5f55a6a945e31ab166adb0a2f9b09
ivanstepatchev <i1nes@icloud.com>
2018-07-28 00:20:09 +0400

    1.3. JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j.calculate;
 
+/**
+* Класс вывода а консоль "Hello World!"
+* @author stepachev
+* @since 28.07.2018
+* @version 1 
+*/
+
 public class Calculate {
+	
+/**
+* Main.
+* @param args - args.
+*/
+
 	
 public static void main (String args []){
 	
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ivan Stepachev (mailto:i1nes@icloud.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
