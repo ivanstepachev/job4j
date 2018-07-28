9719e7779bc6de083662bd7d709e212f9d3754ab
ivanstepatchev <i1nes@icloud.com>
2018-07-28 12:15:31 +0400

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,23 +1,27 @@
 package ru.job4j.calculate;
 
 /**
-* Класс вывода а консоль "Hello World!"
-* @author stepachev
-* @since 28.07.2018
-* @version 1 
-*/
-
+ * Calculate.
+ *
+ * @author Ivan Stepachev (mailto:i1nes@icloud.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
-	
-/**
-* Main.
-* @param args - args.
-*/
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
-	
-public static void main (String args []){
-	
-	System.out.println("Hello World!");
-	
-}
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
