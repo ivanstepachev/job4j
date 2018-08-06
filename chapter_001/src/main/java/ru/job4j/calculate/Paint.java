86409500873fdf3fe70349e34bb80420dc4fccf4
ivanstepatchev <i1nes@icloud.com>
2018-08-06 22:20:29 +0400

    5.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/calculate/Paint.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Paint.java
@@ -1,45 +1,43 @@
 package ru.job4j.calculate;
 
+import java.util.function.BiPredicate;
+
+/**
+ * @author Ivan Stepachev (mailto:i1nes@icloud.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Paint {
 
     public String rightTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int width = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
 
     public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int width = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= width - column - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-    return screen.toString();
+       return this.loopBy(
+               height,
+               height,
+               (row, column) -> row >= height - column - 1
+       );
     }
 
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+         );
+    }
+
+    private String loopBy(int height, int width, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int width = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != width; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -50,11 +48,4 @@ public class Paint {
         return screen.toString();
     }
 
-
-
-    public static void main(String[] args)  {
-        Paint paint = new Paint();
-
-        System.out.println(paint.pyramid(4));
-}
 }
