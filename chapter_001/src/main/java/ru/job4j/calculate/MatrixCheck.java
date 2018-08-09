3bf889c3d427f6bd1f58044e16bb6a5a5c4bb035
ivanstepatchev <i1nes@icloud.com>
2018-08-10 01:27:32 +0400

    6.6. Двухмерный массив. Таблица умножения. [#33491]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/MatrixCheck.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculate;
+
+public class MatrixCheck {
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for (int i = 0; i != data.length; i++) {
+            if (data[0][0] != data[i][i] || data[0][data.length - 1] != data[i][data.length - i - 1]) {
+                result = false;
+            }
+        }
+return result;
+    }
+}
