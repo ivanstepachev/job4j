aaf8672633be92131d6128f2bdce08ce126e720d
ivanstepatchev <i1nes@icloud.com>
2018-08-09 23:53:54 +0400

    6.4. Слова начинается с ... [#41585]

--- chapter_001/src/main/java/ru/job4j/calculate/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/calculate/ArrayChar.java
@@ -14,18 +14,17 @@ public class ArrayChar {
      * @param prefix префикс.
      * @return если слово начинается с префикса.
      */
+
     public boolean startWith(String prefix) {
-    boolean result = true;
-    char[] value = prefix.toCharArray();
-    for (int indexData = 0; indexData < data.length; indexData++) {
-    }
-    for (int indexValue = 0; indexValue < value.length; indexValue++) {
-    }
-    if ((data[0] != value[0]) || (data[1] != value[1])) {
-        result = false;
-    }
-    return result;
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] != data[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
-
 
