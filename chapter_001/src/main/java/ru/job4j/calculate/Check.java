069fbf4212d5423dcc04924cad8a000022912f67
ivanstepatchev <i1nes@icloud.com>
2018-08-09 15:46:05 +0400

    6.3. Массив заполнен true или false [#53857]

--- chapter_001/src/main/java/ru/job4j/calculate/Check.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Check.java
@@ -3,22 +3,13 @@ package ru.job4j.calculate;
 public class Check {
 
     public boolean mono(boolean[] data) {
-       boolean result = true;
-       boolean hasTrue = false;
-       boolean hasFalse = false;
-       for (int index = 0; index < data.length; index++) {
-           if (!data[index]) {
-               hasFalse = true;
-               if (hasTrue) {  // массив имеет и True, и False
-                   result = false;
-               }
-           } else {
-               hasTrue = true;
-               if (hasFalse) { //массив имеет и True, и False
-                   result = false;
-               }
-           }
-       }
-        return result;  //либо только True, либо False
-        }
+        boolean result = true;
+        for (int i = 0; i != data.length; i++) {
+            for (int j = i + 1; j != data.length; j++) {
+                if (data[i] != data[j]) {
+                    result = false;
+                }
+            }
+        } return result;
+    }
 }
