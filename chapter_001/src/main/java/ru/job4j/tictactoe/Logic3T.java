ac24fbdbdb881117b5a095d8bdec55a466cf0f99
ivanstepatchev <i1nes@icloud.com>
2018-08-17 23:40:24 +0400

    Крестики-нолики на JavaFX [#49777]

---
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -0,0 +1,51 @@
+package ru.job4j.tictactoe;
+
+public class Logic3T {
+    private final Figure3T[][] table;
+
+    public Logic3T(Figure3T[][] table) {
+        this.table = table;
+    }
+
+
+    public boolean isWinnerX() {
+        boolean result = false;
+        if ((table[0][0].hasMarkX() && table[1][1].hasMarkX() && table[2][2].hasMarkX())
+                || (table[2][0].hasMarkX() && table[1][1].hasMarkX() && table[0][2].hasMarkX())
+                || (table[0][0].hasMarkX() && table[1][0].hasMarkX() && table[2][0].hasMarkX())
+                || (table[0][1].hasMarkX() && table[1][1].hasMarkX() && table[2][1].hasMarkX())
+                || (table[0][2].hasMarkX() && table[1][2].hasMarkX() && table[2][2].hasMarkX())
+                || (table[0][0].hasMarkX() && table[0][1].hasMarkX() && table[0][2].hasMarkX())
+                || (table[1][0].hasMarkX() && table[1][1].hasMarkX() && table[1][2].hasMarkX())
+                || (table[2][0].hasMarkX() && table[2][1].hasMarkX() && table[2][2].hasMarkX())) {
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean isWinnerO() {
+        boolean result = false;
+        if ((table[0][0].hasMarkO() && table[1][1].hasMarkO() && table[2][2].hasMarkO())
+                || (table[2][0].hasMarkO() && table[1][1].hasMarkO() && table[0][2].hasMarkO())
+                || (table[0][0].hasMarkO() && table[1][0].hasMarkO() && table[2][0].hasMarkO())
+                || (table[0][1].hasMarkO() && table[1][1].hasMarkO() && table[2][1].hasMarkO())
+                || (table[0][2].hasMarkO() && table[1][2].hasMarkO() && table[2][2].hasMarkO())
+                || (table[0][0].hasMarkO() && table[0][1].hasMarkO() && table[0][2].hasMarkO())
+                || (table[1][0].hasMarkO() && table[1][1].hasMarkO() && table[1][2].hasMarkO())
+                || (table[2][0].hasMarkO() && table[2][1].hasMarkO() && table[2][2].hasMarkO())) {
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean hasGap() {
+        boolean result = false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (!table[i][j].hasMarkO() & !table[i][j].hasMarkX()) {
+                    result = true;
+                }
+            }
+        } return result;
+    }
+}
